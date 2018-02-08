249edcb75ea2610db9345b610d96d0210fb5111b
indegow <indegow@aol.com>
2018-02-07 20:32:41 -0800

    passing more tests

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -12,7 +12,8 @@ var _ = require('underscore');
 exports.paths = {
   siteAssets: path.join(__dirname, '../web/public'),
   archivedSites: path.join(__dirname, '../archives/sites'),
-  list: path.join(__dirname, '../archives/sites.txt')
+  list: path.join(__dirname, '../archives/sites.txt'),
+  index: path.join(__dirname, '../web/public/index.html')
 };
 
 // Used for stubbing paths for tests, do not modify
@@ -26,15 +27,27 @@ exports.initialize = function(pathsObj) {
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback) {
+  fs.readFile(exports.paths.list, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      callback(data.toString().split('\n'));
+    }
+  });
 };
 
 exports.isUrlInList = function(url, callback) {
+  exports.readListOfUrls(data => {
+    callback(data.includes(url));
+  });
 };
 
 exports.addUrlToList = function(url, callback) {
+  fs.writeFile(exports.paths.list, url, 'utf8', callback);
 };
 
 exports.isUrlArchived = function(url, callback) {
+
 };
 
 exports.downloadUrls = function(urls) {
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,40 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var httpHelper = require('./http-helpers');
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  
+  if (req.method === 'GET') {
+    res.setHeader('Content-Type', 'application/JSON');
+    
+    if (req.url === '/') {
+      var asset = archive.paths.index;
+      httpHelper.serveAssets(res, asset); // no func passed in....
+    } else {
+      var asset = `${archive.paths.archivedSites}/${req.url}`;
+      httpHelper.serveAssets(res, asset);
+    }
+  }
+
+
+  if (req.method === 'POST') {
+    req.on('data', (data) => {
+      var url = data.toString().slice(4);
+      console.log(url);
+      res.writeHead(302, url);
+      archive.isUrlInList(url, (exists) => {
+        if (!exists) {
+          archive.addUrlToList(url);
+        } else {
+          var asset = `${archive.paths.archivedSites}/${url}`;
+          httpHelper.serveAssets(res, asset);
+        }
+      });
+    });
+
+
+  }
+
+  //res.end(archive.paths.list);
 };
